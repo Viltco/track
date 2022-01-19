cda3da0dcedf55f6f9584aee9bf8f5cc3309b7f9
Viltco <58694110+Viltco@users.noreply.github.com>
2022-01-19 17:02:36 +0500

    warehouse

---
+++ sale_order_line_by_warehouse_app/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Sale Order Multi Warehouse App",
+    "author": "SolutionFounder",
+    "version": "14.0.1.0",
+    'summary': 'Sale order line by warehouse sale order multiple warehouse sale order multiple warehouse sale multi warehouse sales multi warehouse by sale order line warehouse by sale line wise warehouse sale order line wise warehouse on sale order line warehouse select',
+    "description": """ This module Helps To Create Delivery Order According To Related Warehouse. """,
+    "category": "Sales",
+    "depends": ['base', 'sale_management', 'stock'],
+    "data": [
+            'views/sale_config_settings.xml',
+            'views/sale_order_line_by_warehouse.xml', 
+    ],
+    "auto_install": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
